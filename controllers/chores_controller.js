eaba4d64f177eae0bd1f245c1d6dc501d67da263
RyanJRasmussen <ryan.james.rasmussen@gmail.com>
2022-05-14 14:19:13 -0500

    Added sequelize setup

--- controllers/chores_controller.js
+++ controllers/chores_controller.js
@@ -1,21 +1,25 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Testing general get');
+});
 
 router.get('/testing', (req, res) => {
-	res.send('GsdfsdfETTesting');
-	console.log('Testing');
+  res.send('GsdfsdfETTesting');
+  console.log('Testing');
 });
 
 router.post('/testing', (req, res) => {
-	res.send('postTesting');
+  res.send('postTesting');
 });
 
 router.put('/testing', (req, res) => {
-	res.send('puttesting');
+  res.send('puttesting');
 });
 
 router.delete('/testing', (req, res) => {
-	res.send('deleattesting');
+  res.send('deletetesting');
 });
 
-module.exports = router
+module.exports = router;
