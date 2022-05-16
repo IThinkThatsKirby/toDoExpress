6e483ffc7f0a58c357035676d26fa5dff967aef1
kirby <hkellingson@gmail.com>
2022-05-16 15:36:44 -0500

    import routes and functions !!!!

--- server.js
+++ server.js
@@ -1,26 +1,11 @@
 //dependencies
+require('dotenv').config();
 const express = require('express');
-const db = require('./db/queries');
 const app = express();
 const PORT = process.env.PORT;
-const PG_CONNECTION = process.env.PG_CONNECTION;
-// Database things go here REMEBER TO FIX THE VARIABLES before you DEPLOY
-const { Client } = require('pg');
-const client = new Client({
-	connectionString: PG_CONNECTION,
-	ssl: {
-		rejectUnauthorized: false,
-	},
-});
-
+// make connection
+const { client, testGet } = require('./db/index');
 client.connect();
-
-client.query('SELECT * FROM actor;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-});
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -30,20 +15,17 @@ const choresController = require('./controllers/chores_controller.js');
 app.use('/chores', choresController);
 
 // Routes
-app.get('/', (req, res) => {
-  client.query('SELECT * FROM actor;', (err, res) => {
-    console.log(res.rows);
-    return res.rows;
-  });
-  res.send(JSON.stringify(res.row));
-});
+app.get('/', async (req, res) => {
+	const { rows } = await client.query('SELECT * FROM actor;');
 
-app.get('*', (req, res) => {
-  res.send('Error 404');
+	res.send(rows[0]);
 });
-app.post('/', db.createChore);
+app.get('/1', testGet);
+app.get('*', (req, res) => {
+	res.send('Error 404');
+});
 
 // Listen
 app.listen(PORT, () => {
-  console.log(`ALIVE ${PORT}`);
+	console.log(`ALIVE ${PORT}`);
 });
